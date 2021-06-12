349084e55f32de6d2fbbb8dac7732b48ba4561d2
Yusuke Yoshino <y.yoshino1011@gmail.com>
2021-06-12 15:49:14 -0700

    add GET single todo route

--- server/index.js
+++ server/index.js
@@ -37,6 +37,16 @@ app.get("/todos", async(req,res)=>{
 
 // get a todo
 
+app.get("/todos/:id",async (req,res)=>{
+  try {
+    const {id} = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",[id]);
+    res.json(todo.rows);
+  } catch(err){
+    console.error(err);
+  }
+})
+
 // update a todo
 
 app.listen(5000, () => {
