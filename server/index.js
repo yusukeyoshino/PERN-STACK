90bec9ef5f63ad1b084fc2ed5b249936530857ec
Yusuke Yoshino <y.yoshino1011@gmail.com>
2021-06-12 15:37:52 -0700

    set POST route

--- server/index.js
+++ server/index.js
@@ -17,8 +17,8 @@ app.use(express.json());
 app.post("/todos", async(req,res)=>{
   try {
     const {description} = req.body;
-    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)",[description])
-    res.json(newTodo);
+    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",[description])
+    res.json(newTodo.rows);
   } catch(err) {
     console.error(err);
   }
